a4ec5bc3d9a1af311b0c7732e51f2b57e46add30
sapient <sridharan11152@gmail.com>
2018-02-23 10:16:19 +0530

    added extra test cases

--- src/test/java/com/dbEngine/project/dbEngine/queryclTest.java
+++ src/test/java/com/dbEngine/project/dbEngine/queryclTest.java
@@ -13,51 +13,65 @@ import junit.framework.Assert;
 @RunWith(JUnitPlatform.class)
 
 class queryclTest {
-
+	querycl obj = new querycl();
+	Dbtest obj1 = new Dbtest();
+	String query ="select * from ipl.csv where season > 2014 and city = Bangalore order by win_by_runs group by team1";
+	String[] spl_qry=obj.splqry(query);
+	
 	@BeforeEach
-	void setUp() throws Exception {
-	}
+	void setUp() throws Exception 
+	     {
+		System.out.println("select * from ipl.csv where season > 2014 and city = Bangalore order by win_by_runs group by team1");
+	     }
 
 	@Test
-	void test() {
-		// To test the filename
-		querycl obj = new querycl();
-    	Dbtest obj1 = new Dbtest();
-    	String query ="select * from ipl.csv where season > 2014 and city = Bangalore order by win_by_runs group by team1";
-        String[] spl_qry=obj.splqry(query);
-    	String file_name = obj.extfname(spl_qry);
+	void test1() // To test the filename extracting method
+	     {
+		
+		String file_name = obj.extfname(spl_qry);
     	assertEquals("ipl.csv",file_name,"incorrect filename");
+	     }
     	
-    	/*// To test the base part 
+    void test2() // To test the basepart extracting method
+         {
     	String base_part = obj.splwhr(spl_qry);
     	assertEquals("select*fromipl.csv",base_part,"incorrect basepart");
-    	
-    	// To test the filter part
+    	 }
+    void test3() // To test the filter part extracting method
+        {
     	String filter_part = obj.aftwhr(spl_qry);
     	assertEquals("season>2014andcity=Bangaloreorderbywin_by_runsgroupbyteam1",filter_part,"incorrect filterpart");
-    	
-        // To check the conditions
+    	} 
+    void test4() // To check the conditions extracting method
+        {
     	String []conditions= {"","","",""};
     	conditions = obj.restrictions(spl_qry,query);
     	assertEquals("season>2014",conditions[0],"incorrect conditions");
     	assertEquals("city=Bangalore",conditions[1],"incorrect conditions");
-    	// To check the logical operators 
+    	}
+    void test5() // To check the logical operators extracting method
+    	{
     	String []ops= {"","","",""};
     	ops = obj.logical_ops(spl_qry,query);
     	assertEquals("and",ops[0],"incorrect logical operators");
-    	// To check the group by field
+    	}
+    void test6() // To check the group by field extracting method
+    	{
     	String group= "";
     	group= obj.grpfld(spl_qry,query);
     	assertEquals("team1",group,"incorrect group by field");
-    	// To check the order by field
-    	String order= "";
+    	}
+    void test7() // To check the order by field extracting method
+    	{
+    		String order= "";
     	order= obj.ordfld(spl_qry,query);
     	assertEquals("win_by_runs",order,"incorrect order by field");
-    	
-    	
-    	*/
-    	
-    	
-	}
-
+    	}
 }
+    	
+    	
+    	
+    	
+	
+
+
